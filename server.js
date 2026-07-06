2c51b946120f7f751d676b773382916e37657822
Tayler Caufield <taylercaufield@gmail.com>
2026-07-05 17:49:50 -0700

    server.js set

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const scoreRoutes = require('./routes/score');
+const leaderboardRoutes = require('./routes/leaderboard');
+
 
 const app = express();
+const port = 3000; 
+
 app.use(express.json());
+app.use('/api/scores', scoreRoutes);
+app.use('/api/leaderboard', leaderboardRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
